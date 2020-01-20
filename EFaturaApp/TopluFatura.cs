6d1817325504a064bdc03dfacbd6ea0d26d1174b
Mehmet ÖNELGE <monelge@gmail.com>
2020-01-20 23:50:09 +0300

    .

--- EFaturaApp/TopluFatura.cs
+++ EFaturaApp/TopluFatura.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EFaturaApp.EntFM;
 
 namespace EFaturaApp
 {
     public partial class TopluFatura : BaseForm
     {
+        EKSPRES2017Entities dbEntities = new EKSPRES2017Entities();
         public TopluFatura()
         {
             InitializeComponent();
@@ -19,7 +21,11 @@ namespace EFaturaApp
 
         void sayfaYukle()
         {
+            var subeListesi = dbEntities.krhatsub.ToList();
 
+            radDropDownList2.DataSource = subeListesi;
+            radDropDownList2.ValueMember = "sube";
+            radDropDownList2.DisplayMember = "subeismi";
         }
         private void TopluFatura_Load(object sender, EventArgs e)
         {
